5152d27edd0e6c80c292ae797b5ad0d97a9b0e41
Bobby <i@shuibo.cn>
2019-04-03 11:22:48 +0800

    Spring Boot 简介 & 入门

---
+++ 01.Spring-Boot-Open/src/main/java/cn/shuibo/SpringBootOpenApplication.java
@@ -0,0 +1,13 @@
+package cn.shuibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpenApplication.class, args);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SpringAll
-Spring Boot &amp; Spring Cloud 学习整理记录
+Spring Boot &amp; Spring Cloud 学习记录整理  &amp; http://shuibo.cn
+
+#### Spring Boot教程
+01.[Spring Boot 入门](https://shuibo.cn/01-spring-boot-open.html) 
